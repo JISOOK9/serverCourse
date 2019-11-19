c019373847a2316da8c5feefbb2fd52f31cde31b
JISOOK9 <51140876+JISOOK9@users.noreply.github.com>
2019-11-19 15:15:44 +0900

    Update reqController.js

--- reqController.js
+++ reqController.js
@@ -1,6 +1,18 @@
 const resController = {};
 const util = require("./util")
 
+//db연결
+const mysql = require('mysql');
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'admin',
+    database: 'test',
+    connectionLimit: 10, //풀 내에 커넥션 개수 제한
+    debug: false
+})
+
 const sqlConfig = require('./sql_config');
 
 resController.list = (req, res)=> {
@@ -22,4 +34,35 @@ resController.search = (req, res)=>{
     pool.getConnection((err, conn)=>{util.runQuery(sql, ['%' + params.name + '%'], res, err, conn)
     });
 }
+
+resController.insert = (req, res)=>{
+    console.log('/user/add is requested');
+    const params = req.query;
+    console.log('PARAMS-> ' + JSON.stringify(params));
+    let sql = sqlConfig.user.add;
+    pool.getConnection((err, conn)=>{util.runQuery(sql, [params.name, params.age], res, err, conn)
+    });
+}
+
+//id를 넘기기
+resController.update = (req, res)=>{
+    console.log('/user/modify is requested');
+    const params = req.query;
+    console.log('PARAMS-> ' + JSON.stringify(params));
+    
+    let sql = sqlConfig.user.update;
+    
+    pool.getConnection((err, conn)=>{util.runQuery(sql, [params.name, params.age, params.id], res, err, conn)
+    });
+}
+
+resController.delete = (req, res)=>{
+    console.log('/user/remove is requested');
+    const params = req.query;
+    console.log('PARAMS-> ' + JSON.stringify(params));
+    let sql = sqlConfig.user.remove;
+    pool.getConnection((err, conn)=>{util.runQuery(sql, [params.id], res, err, conn)
+    });
+}
+
 module.exports = resController;
